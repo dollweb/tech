1a04dc01558902ddea125c0972f24b320919bbe0
dollweb <101566500+dollweb@users.noreply.github.com>
2024-01-27 12:49:07 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -67,9 +67,9 @@ with col1:
          with tf.compat.v1.Session() as sess:
             sess.run(model_tf)
             # save_path = 'C:/research/saved.cpkt'
-            # save_path = '/content/drive/MyDrive/research/saved.cpkt'
+            save_path = '/content/drive/MyDrive/research/saved.cpkt'
             # save_path = 'https://raw.githubusercontent.com/dollweb/tech/main/'
-            save_path = 'https://raw.githubusercontent.com/dollweb/tech/main/saved.cpkt'
+            # save_path = 'https://raw.githubusercontent.com/dollweb/tech/main/saved.cpkt'
             # save_path = "https://raw.githubusercontent.com/dollweb/tech/main/saved.cpkt"
             # save_path = 'https://raw.githubusercontent.com/dollweb/tech/main/checkpoint.cpkt'
             saver.restore(sess, save_path)
