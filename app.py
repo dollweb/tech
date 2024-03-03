1cf0ed50c2714c7bacebc11a9df81f160e443fbc
dollweb <101566500+dollweb@users.noreply.github.com>
2024-03-03 19:16:43 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -23,16 +23,16 @@ st.subheader("기술정보를 입력해주세요")
 st.markdown("**기술정보**를 입력해주세요")
 st.text("기술정보를 입력해주세요")
 
-url = 'https://raw.githubusercontent.com/dollweb/tech/main/창업기업평가_하나은행_2022_원본_06_10개독립변수.csv'
+url = 'https://raw.githubusercontent.com/dollweb/tech/main/창업기업평가_하나은행_2022_원본_10_10개독립변수.csv'
 encoded_url = urllib.parse.quote(url, safe=':/')
 df = pd.read_csv(encoded_url)
 y = df.T_SC
 tf.compat.v1.disable_eager_execution()
 tf.compat.v1.global_variables_initializer()
 model_tf = tf.compat.v1.global_variables_initializer()
-X_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, 10])
+X_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, 16])
 y_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
-W = tf.Variable(tf.random.normal([10, 1]), name="weight") # 가중치 초기화
+W = tf.Variable(tf.random.normal([16, 1]), name="weight") # 가중치 초기화
 b = tf.Variable(tf.random.normal([1]), name="bias") # 바이어스값 초기화
 hypothesis = tf.matmul(X_tf, W) + b
 saver = tf.compat.v1.train.Saver()
@@ -43,16 +43,22 @@ col1, col2 = st.columns(2)
 with col1:
    st.subheader("기술정보입력")
    with st.form("form1"):
+      T_14_temp = st.text_input('자본조달능력: ')
+      T_19_temp = st.text_input('모방의난이도: ')
+      T_24_temp = st.text_input('시장성장성: ')
+      T_26_temp = st.text_input('인지도: ')
+      T_28_temp = st.text_input('경쟁제품과비교우위성: ')
       T_32_temp = st.text_input('종업원수: ')
       T_39_temp = st.text_input('동업종종사기간: ')
       T_43_temp = st.text_input('연구개발조직운영기간: ')
-      T_46_temp = st.text_input('기술인력수: ')
-      T_50_temp = st.text_input('특허등록수: ')
+      T_47_temp = st.text_input('인증누적건수: ')
+      T_79_temp = st.text_input('업력: ')
       T_80_temp = st.text_input('벤처+이노비즈: ')
+      T_82_temp = st.text_input('지식재산권전체합계: ')
       T_83_temp = st.text_input('매출액2년평균: ')
-      T_88_temp = st.text_input('영업이익3년평균: ')
-      T_106_temp = st.text_input('연구소디자인더미: ')
-      T_110_temp = st.text_input('3개년개발비더미: ')
+      T_101_temp = st.text_input('연구개발비2년평균: ')
+      T_111_temp = st.text_input('연구소디자인더미: ')
+      T_115_temp = st.text_input('3개년개발비더미: ')
 
       submit = st.form_submit_button("확인")
       등급 = ""
@@ -67,10 +73,10 @@ with col1:
                f.write(response.content)
             saver.restore(sess, save_path)
             scaler_T_SC = MinMaxScaler()
-            input_data = np.array([[T_32_temp, T_39_temp, T_43_temp, T_46_temp, T_50_temp, T_80_temp, T_83_temp, T_88_temp, T_106_temp, T_110_temp]]).reshape(1, -1)
+            input_data = np.array([[T_14_temp, T_19_temp, T_24_temp, T_26_temp, T_28_temp, T_32_temp, T_39_temp, T_43_temp, T_47_temp, T_79_temp, T_80_temp, T_82_temp, T_83_temp, T_101_temp, T_111_temp, T_115_temp]]).reshape(1, -1)
             input_data_normalized = scaler_T_SC.fit_transform(input_data)
             arr = np.array(input_data_normalized, dtype=np.float32)
-            x_data = arr[0:10]
+            x_data = arr[0:16]
             prediction = sess.run(hypothesis, feed_dict={X_tf: x_data})
             predicted_T_SC_NM = prediction[0][0]
             print(f"T_SC_NM 예측된 정규화값: {predicted_T_SC_NM}")
